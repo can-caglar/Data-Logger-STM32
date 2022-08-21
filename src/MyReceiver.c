3fd9afdda0aa37825194e1c54db9be8204f1a946
Can Caglar <charlerjan@gmail.com>
2022-08-21 18:05:11 +0100

    test: add buffer filling tests to MyReceiver

--- src/MyReceiver.c
+++ src/MyReceiver.c
@@ -1,9 +1,18 @@
 #include "MyReceiver.h"
 #include "MyGPIO.h"
 #include "MyUSART.h"
+#include <string.h>
 
 static MyGPIO uartGpio;
 static USART_TypeDef* rcvrUsart;
+
+#define MAX_CHARACTERS 11
+
+static struct
+{
+    char character[MAX_CHARACTERS];
+    unsigned int index;
+} inbuf;
 
 void MyReceiver_Init(USART_TypeDef* usart,
                      GPIO_TypeDef* gpio, 
@@ -18,14 +27,32 @@ void MyReceiver_Init(USART_TypeDef* usart,
     uartGpio.output_type = GPIO_PUSH_PULL;
     uartGpio.pupd = GPIO_PUPD_UP;
     MyGPIO_Init(&uartGpio);
+    MyUSART_Init(usart, USART_BR_19200);
 
     rcvrUsart = usart;
-    MyUSART_Init(usart, USART_BR_19200);
+    memset(&inbuf, 0, sizeof(inbuf));
 }
 
 ReceiverEcode_e MyReceiver_Receive(void)
 {
     char data = 0;
-    MyUSART_Read(rcvrUsart, &data);
-    return data;
+    if (MyUSART_Read(rcvrUsart, &data) == ECODE_OK)
+    {
+        if (inbuf.index < (MAX_CHARACTERS - 1))
+        {
+            if (data != '\n')
+            {
+                inbuf.character[inbuf.index++] = data;
+                return RCVR_RECEIVED;
+            }
+        }
+        return RCVR_DONE;
+        
+    }
+    return RCVR_NOT_RECEIVED;
+}
+
+char* MyReceiver_GetBuffer(void)
+{
+    return inbuf.character;
 }
--- test/test_MyReceiver.c
+++ test/test_MyReceiver.c
@@ -6,6 +6,12 @@
 #include "unity_helper.h"
 #include <stdio.h>
 
+void initMyReceiver(void);
+void initMyReceiverNoExpectations(void);
+ReceiverEcode_e receiveFromUsart(char byte);
+ReceiverEcode_e receiveFromUsart_String(char* string);
+ReceiverEcode_e receiveFromUsart_Fail(void);
+
 void setUp(void)
 {
 
@@ -31,18 +37,125 @@ void test_MyReceiver_Init_WillInitialiseUSARTAndGPIOProperly(void)
     MyGPIO_Init_ExpectAndReturn(&expectedStruct, ECODE_OK);
     MyUSART_Init_ExpectAndReturn(MY_USART, USART_BR_19200, ECODE_OK);
 
+    initMyReceiver();
+}
+
+void test_MyReceiver_Receive_WillReadFromUSARTEvenIfNotInitialised(void)
+{
+    char readData = 0;  // should pass in a 0
+    MyUSART_Read_ExpectAndReturn(MY_USART, &readData, ECODE_OK);
+
+    MyReceiver_Receive();
+}
+
+void test_MyReceiver_GetBuffer_ReceivedCharsAndEmptiedAfterInit(void)
+{
+    initMyReceiverNoExpectations();
+
+    // empty after init
+    char* buf = MyReceiver_GetBuffer();
+    TEST_ASSERT_EQUAL_STRING("", buf);
+
+    // not empty after receiving
+    ReceiverEcode_e err = receiveFromUsart('a');
+    TEST_ASSERT_EQUAL_INT(RCVR_RECEIVED, err);
+    buf = MyReceiver_GetBuffer();
+    TEST_ASSERT_EQUAL_STRING("a", buf);
+
+    // empty again after init!
+    initMyReceiverNoExpectations();
+    buf = MyReceiver_GetBuffer();
+    TEST_ASSERT_EQUAL_STRING("", buf);
+}
+
+void test_MyReceiver_GetBuffer_FillsUpAfter10Characters(void)
+{
+    initMyReceiverNoExpectations();
+
+    ReceiverEcode_e err = receiveFromUsart_String("0123456789abc");
+    TEST_ASSERT_EQUAL_INT(RCVR_DONE, err);
+
+    char* buf = MyReceiver_GetBuffer();
+    TEST_ASSERT_EQUAL_STRING("0123456789", buf);
+}
+
+void test_MyReceiver_Returns_RECEIVED_EvenIfErrorAsLongAsReceivedOnce(void)
+{
+    initMyReceiverNoExpectations();
+
+    ReceiverEcode_e err = receiveFromUsart_Fail();
+    TEST_ASSERT_EQUAL_INT(RCVR_NOT_RECEIVED, err);
+
+    err = receiveFromUsart('a');
+    TEST_ASSERT_EQUAL_INT(RCVR_RECEIVED, err);
+
+    err = receiveFromUsart_Fail();
+    TEST_ASSERT_EQUAL_INT(RCVR_NOT_RECEIVED, err);
+
+}
+
+void test_MyReceiver_GetBuffer_FillsUpAfterReceivingReturn(void)
+{
+    initMyReceiverNoExpectations();
+
+    ReceiverEcode_e err = receiveFromUsart_String("012\n345");
+    TEST_ASSERT_EQUAL_INT(RCVR_DONE, err);
+
+    char* buf = MyReceiver_GetBuffer();
+    TEST_ASSERT_EQUAL_STRING("012", buf);
+}
+
+/******************************** Helper Functions *****************/
+
+void initMyReceiver(void)
+{
     MyReceiver_Init(MY_USART, MY_USART_GPIO, MY_USART_ALT, MY_USART_RX, MY_USART_TX);
 }
 
-void test_MyReceiver_Receive_WillReadFromUSART(void)
+void initMyReceiverNoExpectations(void)
+{
+    MyGPIO_Init_IgnoreAndReturn(ECODE_OK);
+    MyUSART_Init_IgnoreAndReturn(ECODE_OK);
+    MyReceiver_Init(MY_USART, MY_USART_GPIO, MY_USART_ALT, MY_USART_RX, MY_USART_TX);
+}
+
+ReceiverEcode_e receiveFromUsart(char byte)
 {
     char readData = 0;
-    char retData = 'a';
     MyUSART_Read_ExpectAndReturn(MY_USART, &readData, ECODE_OK);
-    MyUSART_Read_ReturnThruPtr_readData(&retData);
-    
-    char actualData = MyReceiver_Receive();
+    MyUSART_Read_ReturnThruPtr_readData(&byte);
 
-    TEST_ASSERT_EQUAL_CHAR(retData, actualData);
-
+    return MyReceiver_Receive();
 }
+
+ReceiverEcode_e receiveFromUsart_Fail(void)
+{
+    char readData = 0;
+    MyUSART_Read_ExpectAndReturn(MY_USART, &readData, ECODE_NOT_READY);
+
+    return MyReceiver_Receive();
+}
+
+ReceiverEcode_e receiveFromUsart_String(char* string)
+{
+    ReceiverEcode_e err = RCVR_NOT_RECEIVED;
+    while (*string)
+    {
+        char readData = 0;
+        MyUSART_Read_ExpectAndReturn(MY_USART, &readData, ECODE_OK);
+        MyUSART_Read_ReturnThruPtr_readData(string);
+
+        err = MyReceiver_Receive();
+        if (err != RCVR_RECEIVED)
+        {
+            break;
+        }
+        string++;
+    }
+    return err;
+}
+
+/*
+- [x] MyReceiver_Receive will return 0 if received 0 and buffer is empty
+- [x] MyReceiver_Receive will continue to rreturn RECEIVED even if uart fails
+*/
