b5c83417ad16a79ba4e909ad260051e430af585f
Can Caglar <charlerjan@gmail.com>
2022-11-20 15:16:09 +0000

    chore: change project name to Serial Snooper

--- RTE/_Target_test/RTE_Components.h
+++ RTE/_Target_test/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'MyHAL' 
+ * Project: 'SerialSnooper' 
  * Target:  'Target_test' 
  */
 
