3c73c291497611883dad035abdf81a1702a5ea30
Can Caglar <charlerjan@gmail.com>
2023-02-23 16:08:51 +0000

    testing speed of filling up circular buffer with Teraterm

--- src/MyApp.c
+++ src/MyApp.c
@@ -7,12 +7,27 @@
 #include "main.h"
 // perhaps below should be in the other modules only
 #include "MyTimeString.h"
-
+#include "MyCircularBuffer.h"
+#include <stdio.h>
+#include "stm32f3xx.h"
 
 // Ceedling has its own main function
 #ifndef TEST
 #define runApp main
 #endif
+
+#define SAFE_PRINT(str) __disable_irq(); printf(str); __enable_irq()
+#define SAFE_PRINT_ARGS(str, ...) __disable_irq(); printf(str, __VA_ARGS__); __enable_irq()
+
+typedef enum TEST_e
+{
+  TEST_DEFAULT_PROGRAM,
+  TEST_CIRC_BUF,
+} TEST_e;
+
+TEST_e test = TEST_CIRC_BUF;
+uint32_t tNow;
+uint32_t tElapsed;
 
 /*
 int main(void)
@@ -21,23 +36,17 @@ int runApp(void)
 {
     CubeMX_SystemInit(CMX_FATFS);   // needs a 1s delay
     AppDecider_Init();
-    
-    if (AppDecider_Decide() == APP_CLI)
+    CubeMX_SystemInit(CMX_UART);
+    MyScheduler_Init();
+    SystemOperations_Init();
+    MyTimeString_Init();
+      
+    switch (test)
     {
-        // The CLI application
-        MyCLI_Init();
-        LOOP
-        {
-            MyCLI_Run();
-        }
-    }
-    else
-    {
+      case TEST_DEFAULT_PROGRAM:
+      {
         // The Serial Snooping application
-        CubeMX_SystemInit(CMX_UART);
-        MyScheduler_Init();
-        SystemOperations_Init();
-        MyTimeString_Init();
+        
         MyScheduler_AddTask(SystemOperations_OpenLogFile,
             0, true, true);
         MyScheduler_AddTask(SystemOperations_WriteSD, 
@@ -48,5 +57,31 @@ int runApp(void)
         {
             MyScheduler_Run();
         }
-    }
+        break;
+      }
+      
+      case TEST_CIRC_BUF:
+      {
+          SAFE_PRINT("Starting circular buffer test. Blast data via UART!\n\r");
+          MyCircularBuffer_init();
+          uint32_t bufSize = 0;
+          while(MyCircularBuffer_isEmpty()) 
+          {
+              tNow = HAL_GetTick();  // sit here while buf is empty
+          }
+          while (!MyCircularBuffer_isFull())
+          {
+              // sit here while buf is not full
+          }
+          tElapsed = HAL_GetTick() - tNow;
+          bufSize = MyCircularBuffer_size();
+          SAFE_PRINT_ARGS("Time to fill circular buffer %u ms | Total data: %u bytes\n\r", tElapsed, bufSize);
+          break;
+      }
+      default:
+      {
+        printf("No test program chosen\n\r");
+        break;
+      }
+    };
 }
